790bedd681fa5985f5bcd68ef4791f2914caa30e
Gaurav Tinkhede <g.3khede@gmail.com>
2019-06-17 12:22:04 +0300

    Added comments

--- src/main/java/com/tinkhede/backendapi/google/map/models/GoogleMapAPIDataModel.java
+++ src/main/java/com/tinkhede/backendapi/google/map/models/GoogleMapAPIDataModel.java
@@ -1,7 +1,7 @@
 package com.tinkhede.backendapi.google.map.models;
 
 import java.util.List;
-
+//This is a placeholder model which contains data fetched from Google map API
 public class GoogleMapAPIDataModel {
 	
 	private List<GoogleMapAPIModel> results;
